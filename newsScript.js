4026ec5be92c2dfa939ec75486559e76e1ef3a15
FaisalIlyas <faisalilyas2600@gmail.com>
2024-05-25 07:49:10 +0500

    changing in JS file

--- newsScript.js
+++ newsScript.js
@@ -1,1 +1,15 @@
 const API_KEY = "3a935743aad944f88761612c6c748cce";
+const url = "https://newsapi.org/v2/everything?q=";
+
+
+window.addEventListener("load", () => fetch("india"));
+
+async function fetchNews (query) {
+   const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+   const data = res.json();
+   bindData(data.articles);
+}
+
+function bindData(articles) {
+    
+}
